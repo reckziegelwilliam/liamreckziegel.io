21dd3ee26f0692b154ab209a124b5eba125301fe
Liam <reckziegel.william@gmail.com>
2025-12-08 21:13:21 -0800

    fix: update import path in guestbook form
    - Change relative import to absolute @/ import
    - Fixes import after moving to (site) route group

--- app/(site)/guestbook/form.tsx
+++ app/(site)/guestbook/form.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useRef } from 'react';
-import { saveGuestbookEntry } from '../db/actions';
+import { saveGuestbookEntry } from '@/app/db/actions';
 import { useFormStatus } from 'react-dom';
 
 export default function Form() {
